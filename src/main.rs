f7ae3933670ccb1a46b1bb950133b03c5b356f5e
Sebastian Fernandez <seb.flipflip@gmail.com>
2023-01-02 14:28:33 -0600

    removed termion

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod images;
 
 use std::{thread, time};
 use std::io::{stdout, Write};
-use termion::clear;
-use termion::cursor;
+// use termion::clear;
+// use termion::cursor;
 use std::fs;
 
 //ImgSqc138fbf84uw4du844edf <- folder name
@@ -20,7 +20,7 @@ fn main() -> Result<(), images::ImageDataError>{
     let args = args::Args::new();
     commands::prepare_vid(&args.path); //en este punto ya esta la carpeta con todas las fotos
 
-    print!("{}{}", clear::All, cursor::Goto(1, 1));
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     for img in fs::read_dir("ImgSqc138fbf84uw4du844edf").expect("failed to read directory ImgSqc138fbf84uw4du844edf"){
         let img = img.expect("Failed to read file");
         let path = img.path().to_str().unwrap().to_string();
@@ -29,7 +29,9 @@ fn main() -> Result<(), images::ImageDataError>{
         images::to_ascii(pic, &DENSITY, w as u16 , h as u16)?;
         stdout().flush().unwrap();
         thread::sleep(time::Duration::from_millis(50));
-        print!("{}{}", clear::All, cursor::Goto(1, 1));
+        // print!("{}{}", clear::All, cursor::Goto(1, 1));
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
     }
 
     commands::end_program();
